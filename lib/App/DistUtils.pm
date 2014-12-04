3c4a942a67248afa41429ea153c9e19a3d352e67
perlancar <perlancar@gmail.com>
2014-12-04 11:03:07 +0000

    Import of PERLANCAR/App-DistUtils-0.01.tar.gz from CPAN.
    gitpan-cpan-distribution: App-DistUtils
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         PERLANCAR/App-DistUtils-0.01.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

---
+++ lib/App/DistUtils.pm
@@ -0,0 +1,63 @@
+package App::DistUtils;
+
+our $DATE = '2014-12-04'; # DATE
+our $VERSION = '0.01'; # VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Collection of utilities related to Perl distributions
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+App::DistUtils - Collection of utilities related to Perl distributions
+
+=head1 VERSION
+
+This document describes version 0.01 of App::DistUtils (from Perl distribution App-DistUtils), released on 2014-12-04.
+
+=head1 SYNOPSIS
+
+This distribution provides several command-line utilities related to
+Perl distributions.
+
+=head1 SEE ALSO
+
+L<Dist::Util>
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/App-DistUtils>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/perlancar/perl-App-DistUtils>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=App-DistUtils>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+perlancar <perlancar@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by perlancar@cpan.org.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
